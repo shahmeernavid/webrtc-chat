b9f71434329954e3e6d358d7e0d649137bf00a48
Shahmeer Navid <shahmeernavid@gmail.com>
2015-09-16 16:21:54 -0400

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+Peer to peer chat application
+
+# Running Locally
+
+To run on your local machine:
+
+1. Create a virtual environment and start it:
+
+    ```
+    cd chat
+    virtualenv env
+    . env/bin/activate
+    ```
+
+2. Install python dependencies:
+
+    ```
+    pip install -r requirements.txt
+    ```
+
+3. Install node dependencies:
+
+    ```
+    npm i
+    ```
+
+4. Running the app
+
+    ```
+    gulp start
+    ```
---
+++ chat/views.py
@@ -0,0 +1,27 @@
+from chat import app
+import flask
+#shouldn't need below if above import works...
+#from flask import redirect, render_template, url_for, jsonify, request, make_response
+#from flask.ext.socketio import SocketIO, send, emit
+# from app import socketio, render_template
+# because i imported render_template into app, i can import it from app, now
+# that said...is there a better way? I could just do
+# from app import *, but that doesn't seem like good practice?....
+
+@app.route('/ping')
+def ping():
+    return 'Working'
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/<path:path>')
+def fallback(path):
+    print 'fallback'
+    return app.send_static_file('index.html')
+
+# @app.route('/<path:path>')
+# def static_files(path):
+#     return app.send_static_file(path)
+
